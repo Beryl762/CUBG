f89f01dd884f5ea71712dbb5e6c295e89f2f47b3
GuiHunag <305589802@qq.com>
2023-07-06 17:39:16 +0800

    updata

--- Assets/Scripts/Lua/GameLuaMain.lua
+++ Assets/Scripts/Lua/GameLuaMain.lua
@@ -1,14 +1,3 @@
-require ("Global/Global")
-
-GameLuaMain = {}
-function GameLuaMain.Start(self, ...)
-	local cube = UnityEngine.GameObject.Find("Cube")
-	local cube = UnityEngine.GameObject.Find("Text")
-	GameObject.Destroy(cube)
-	print("Start")
-end
-
-function Update()
-end
-
-return GameLuaMain
+version https://git-lfs.github.com/spec/v1
+oid sha256:c574267cc8ad5746652b5385f75fd252cf390be1e7ad129715c5e3cac1c61877
+size 340
---
+++ Assets/Scripts/Lua/UI/UIConfiug.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9786020fd07ac7fc7c26bf9e04ca57c0280747d00ca485ed060a13223519d7c0
+size 129
